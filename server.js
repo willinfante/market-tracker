088633005fe5abd1dc9a990d755e68e1aa27485d
Will Infante <william.j.infante@gmail.com>
2023-02-06 14:47:34 -0500

    start adding chart

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var hostname = '127.0.0.1';
 var port = 3000;
 
 app = express();
-app.use('/static', express.static('public'))
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use('/public', express.static('public'))
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
